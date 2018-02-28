0a9b439a26716677bddf3873ceb9a064a0fb43f8
ertugrul013 <ertugrul013@hotmail.com>
2018-02-28 11:49:04 +0100

    started work on classes

---
+++ Assets/Scripts/DNA.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA {
+
+	bool isMale;
+	int friendliness;
+	int amountOfHealth;
+	float foodDetectionDistance;
+	int age;
+	float strenth;
+	float speed; 
+	string type;
+	float hungerSpeed;
+
+
+#region Gender Decide
+	public bool Gender()
+	{
+		int Choose  = Random.Range(0,1);
+
+		if (Choose > 0.5)
+		{
+			return false;
+		}
+		else if (Choose =< 0.5) 
+		{
+			return true;
+		}
+		else
+		{
+			Debug.Log("YOU FACKED UP BOI");
+		}
+	}
+#endregion
+
+
+}
---
+++ Assets/Scripts/Seks.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seks : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		rand
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
