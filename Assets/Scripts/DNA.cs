e90fcdce09f6dce49aefe24edf27badf43c33099
ertugrul013 <ertugrul013@hotmail.com>
2018-02-28 15:45:25 +0100

    added last scripts

--- Assets/Scripts/DNA.cs
+++ Assets/Scripts/DNA.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DNA {
+	
 
 	bool isMale;
 	int friendliness;
@@ -11,29 +12,62 @@ public class DNA {
 	int age;
 	float strenth;
 	float speed; 
-	string type;
+	bool type;
 	float hungerSpeed;
 
 
 #region Gender Decide
 	public bool Gender()
 	{
-		int Choose  = Random.Range(0,1);
+		float Choose  = Random.Range(0, 1);
 
 		if (Choose > 0.5)
 		{
 			return false;
 		}
-		else if (Choose =< 0.5) 
+		else if (Choose <= 0.5) 
 		{
 			return true;
 		}
 		else
 		{
-			Debug.Log("YOU FACKED UP BOI");
+			Debug.Log("gender couldnt be decided");
+			return null;
 		}
 	}
 #endregion
 
-x
+#region type Decide
+	public bool Types ()
+	{
+		float t  = Random.Range(0, 1);
+
+		if (t > 0.5)
+		{
+			return false;
+		}
+		else if (t <= 0.5)
+		{
+			return true;
+		}
+		else
+		{
+			Debug.Log("no type could be assigend");
+			return null;
+		}
+	}
+#endregion
+
+	public  int foodDetecet(int friend, int amountHealth)
+	{
+		int  detec = foodDetectionDistance;
+		friend = friendliness;
+		amountOfHealth = amountHealth;
+
+		
+
+		return(detec);
+	}
+ 
+
 }
