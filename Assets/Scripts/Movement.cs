cce5681c0d766544d56be4d42600666a999368c3
Jesper Seipenbusch <jesper.s.js@gmail.com>
2018-02-28 15:47:47 +0100

    Movement scripts + material

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+
+	public Vector3 target;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		target = new Vector3(Random.Range(23f, -23f), transform.position.y, Random.Range(23f, -23f));
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float mSpeed = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target, mSpeed);
+
+		if (transform.position == target)
+		{
+			target = new Vector3(Random.Range(23f, -23f), transform.position.y, Random.Range(23f, -23f));
+		}
+	}
+}
