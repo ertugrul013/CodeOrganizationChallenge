3daeb58fd7c5578e5f0b23c6e65d16f192bc9c1d
ertugrul013 <ertugrul013@hotmail.com>
2018-03-02 21:58:03 +0100

    finished on the oop part of the dna

--- Assets/Scripts/DNA.cs
+++ Assets/Scripts/DNA.cs
@@ -13,9 +13,36 @@ public class DNA
 	public float strength;
 	public float health;
 
-	public void GenderTypeDecider()
+	public DNA()//decides the gender and type of the object
 	{
-		
+		isMale = Random.Range(0,2);
+		type = Random.Range(0,2);
+	}
+
+	public DNA(int type)
+	{
+		this.type = type;
+
+		switch (type)
+		{
+			case 0: //the vegan type
+				hunger = Random.Range(60,100);
+				speed = Random.Range(5,10);
+				strength = Random.Range(40,60);
+				health = Random.Range(80,100);
+			break;
+			
+			case 1:
+				hunger = Random.Range(65,100);
+				speed = Random.Range(8,10);
+				strength = Random.Range(45,60);
+				health = Random.Range(70,100);
+			break;
+			
+			default:
+				Debug.Log("IF YOU GOT HERE YOU DID SOMETHING WRONG NOT ME YOU");
+			break;
+		}
 	}
 
 }
