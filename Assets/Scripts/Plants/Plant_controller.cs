4d96ce9d809f1e8e70ddd91d9090d69ac23aedf3
ertugrul013 <ertugrul013@hotmail.com>
2018-03-12 11:52:22 +0100

    latest version for jesper

--- Assets/Scripts/Plants/Plant_controller.cs
+++ Assets/Scripts/Plants/Plant_controller.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
 
 public class Plant_controller : MonoBehaviour {
 
 	//for changing appereance depend on the type
-	public GameObject[] Stages = new GameObject[5];
+	public GameObject[] Stages = new GameObject[3];
 
 	//OOP Var will be handeld automaticly
 	public int type;
@@ -29,7 +28,11 @@ public class Plant_controller : MonoBehaviour {
 	//the max age for now is 100 this will later be adjusted depend on the type and how old the object can get
 	void Growing()
 	{
-		age = age + Time.deltaTime / LifeSpan;		
+		age = age + Time.deltaTime / LifeSpan;
+		if (age <= 10)
+		{
+			
+		}		
 	}
 
 	void GetOOP()
