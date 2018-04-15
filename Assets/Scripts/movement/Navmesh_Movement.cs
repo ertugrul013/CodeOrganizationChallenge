d3d00e0ee90654b1c4bfd64144deb10d8537c246
ertugrul013 <ertugrul013@hotmail.com>
2018-04-15 12:20:19 +0200

    started work on navmeshmovement

---
+++ Assets/Scripts/movement/Navmesh_Movement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navmesh_Movement : MonoBehaviour
+{
+    private NavMeshAgent Agent;
+    private float Speed;
+    private float FOV;
+
+
+}
